430943095e2c657477646d86a78ac9f8cd99bff4
chloeGabriel <chloe-gabriel@outlook.com>
2020-11-04 11:05:24 +0100

    Update Commit

---
+++ js/action.js
@@ -0,0 +1,8 @@
+function cafeMap() {
+    const mapProp = {
+        center: new google.maps.LatLng(51.508742, -0.120850),
+        zoom: 5,
+    };
+    const map = new google.maps.Map(document.getElementById("cafeMap"), mapProp);
+}
+
